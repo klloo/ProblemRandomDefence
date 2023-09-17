c750ede2d69660ea771817b13a191b3d54febcd9
Huiyeong <53117014+klloo@users.noreply.github.com>
2023-09-17 09:47:51 +0900

    타임존 변경

--- src/main/java/com/randps/randomdefence/RandomdefenceApplication.java
+++ src/main/java/com/randps/randomdefence/RandomdefenceApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class RandomdefenceApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(RandomdefenceApplication.class, args);
 	}
 
